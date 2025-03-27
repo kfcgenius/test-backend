9d60cce257e11bf38bc214271f5f64d5b961e2e0
Aleksey <a.e.sokolkov@gmail.com>
2025-03-27 22:17:52 +0500

    fix: test

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
 """Main module with FastAPI routes"""
 
-import os
 import random
 import string
 
-import mysql.connector
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
@@ -19,17 +17,6 @@ app.add_middleware(
 )
 
 
-def get_db_connection():
-    """Returns connection to database"""
-
-    return mysql.connector.connect(
-        host=os.environ["BACKEND_DB_HOST"],
-        user=os.environ["BACKEND_DB_USER"],
-        password=os.environ["BACKEND_DB_PASSWORD"],
-        database=os.environ["BACKEND_DB_NAME"],
-    )
-
-
 @app.get("/strings/random")
 def generate_random_string():
     """Generates random string and returns it as JSON"""
@@ -37,15 +24,5 @@ def generate_random_string():
     alphabet = string.ascii_letters + string.digits
     random_chars = random.choices(alphabet, k=10)  # nosec B311
     random_string = "".join(random_chars)
-
-    db = get_db_connection()
-    cursor = db.cursor()
-    cursor.execute(
-        "CREATE TABLE IF NOT EXISTS strings (id INT AUTO_INCREMENT PRIMARY KEY, value VARCHAR(255))"
-    )
-    cursor.execute("INSERT INTO strings (value) VALUES (%s)", (random_string,))
-    db.commit()
-    cursor.close()
-    db.close()
 
     return {"random_string": random_string}
